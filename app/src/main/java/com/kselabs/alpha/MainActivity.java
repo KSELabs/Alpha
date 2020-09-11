3b8493cee13399bead691a9a716e4b590837f5c9
mongezik19 <MongeziKSELabs19>
2020-09-11 20:12:00 +0200

    Test

--- app/src/main/java/com/kselabs/alpha/MainActivity.java
+++ app/src/main/java/com/kselabs/alpha/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Tesr
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
