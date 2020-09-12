cf9c1563ca0a580fb593f1514f66f0e31e60a0e6
mongezik19 <MongeziKSELabs19>
2020-09-12 08:15:31 +0200

    First Comment under Edit Activity

--- app/src/main/java/com/kselabs/alpha/EditActivity.java
+++ app/src/main/java/com/kselabs/alpha/EditActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EditActivity extends AppCompatActivity {
-
+//This is the activity we want to callupon clicking on any of the items in the recycler view of the menu fragment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
